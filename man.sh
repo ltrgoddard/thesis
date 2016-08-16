c010d33f2f74b3a086e71dd647530fe354f84b08
Louis Goddard <louisgoddard@gmail.com>
2016-08-16 13:12:49 +0100

    counter script update

--- man.sh
+++ man.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 printf 'man <- c('
-grep -En ' (M|m)an( |\. |$)' ../drafts/prynne.txt | grep -Ec "^[0-9]{3}:" | tr -d '\n'
+grep -En ' (M|m)an( |\. |$)' $1 | grep -Ec "^[0-9]{3}:" | tr -d '\n'
 COUNTER=1
 while [  $COUNTER -lt 13  ]; do
     printf ","
-    grep -En ' (M|m)an( |\. |$)' ../drafts/prynne.txt | grep -Ec "^$COUNTER[0-9]{3}:" | tr -d '\n'
+    grep -En ' (M|m)an( |\. |$)' $1 | grep -Ec "^$COUNTER[0-9]{3}:" | tr -d '\n'
     let COUNTER=COUNTER+1
 done
 printf ")\n"
